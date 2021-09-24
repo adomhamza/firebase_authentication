d5319bfd84a01b767522132f6db90bb3df4da416
adomhamza <liveghost93@gmail.com>
2021-09-24 01:35:24 +0000

    FaceBook Auth

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_login_facebook/flutter_login_facebook.dart';
 
 abstract class AuthBase {
   User? get currentUser;
   Future<UserCredential> signInWithGoogle();
-  // Future<User> signInWithFaceBook();
+  Future<User?> signInWithFaceBook();
   Stream<User?> authStateChanges();
   // Future<void> signOut();
 }
@@ -43,6 +44,35 @@ class Auth implements AuthBase {
         code: 'ERROR_ABORTED_BY_USER',
         message: 'SIGN IN ABORTED BY USER',
       );
+    }
+  }
+
+  @override
+  Future<User?> signInWithFaceBook() async {
+    final fb = FacebookLogin();
+    // Log in
+    final res = await fb.logIn(permissions: [
+      FacebookPermission.publicProfile,
+      FacebookPermission.email,
+    ]);
+    switch (res.status) {
+      case FacebookLoginStatus.success:
+        final accessToken = res.accessToken;
+        final userCredential = await _firebaseAuth.signInWithCredential(
+            FacebookAuthProvider.credential(accessToken!.token));
+        return userCredential.user;
+      case FacebookLoginStatus.cancel:
+        throw FirebaseAuthException(
+          code: 'ERROR_ABORTED_BY_USER',
+          message: 'Sign in is aborted by user',
+        );
+      case FacebookLoginStatus.error:
+        throw FirebaseAuthException(
+          code: 'ERROR_FACEBOOK_LOGIN_FAILED',
+          message: res.error!.developerMessage,
+        );
+      default:
+        throw UnimplementedError();
     }
   }
 }
